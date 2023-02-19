72f5adf28c1eb2ebf6b53ad688139633452806e7
Mohammed Essam <memaq1998.10.7@gmail.com>
2023-02-19 19:15:56 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function (a, b) {
+    return a / b;
+};
+const square = (x) => x * x;
+const add = (a, b) => a + b;
+
+console.log(divide(2000, 100));
+console.log(square(2));
+console.log(add(3, 4));
